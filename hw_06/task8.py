894d3fe8bbca96edeb55dd8e843066dfe913a169
olehsierik <o.p.sierik@gmail.com>
2023-07-14 13:36:23 +0300

    edit task8

--- hw_06/task8.py
+++ hw_06/task8.py
@@ -1,6 +1,8 @@
 def is_anagram(text_first, text_two):
     if len(text_first) != len(text_two):
-        return 'Is not anagram'
+        return 'False'
+    if text_first == text_two:
+        return 'False'
 
     text_first = text_first.lower()
     text_two = text_two.lower()
@@ -9,6 +11,6 @@ def is_anagram(text_first, text_two):
     list_first.sort()
     list_two.sort()
     if list_first == list_two:
-        return 'Is anagram'
+        return 'True'
     else:
-        return 'Is not anagram'
+        return 'False'
